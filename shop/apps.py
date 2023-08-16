c60a39011c8c62ce7b8c1fa292f4104609faa4ed
ShokhirevAlexander <142152427+ShokhirevAlexander@users.noreply.github.com>
2023-08-16 21:03:18 +0300

    Delete shop directory

--- shop/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class OlgaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'olga'
-    verbose_name = 'cakes from Olga'
